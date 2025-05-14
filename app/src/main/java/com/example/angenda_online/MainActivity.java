1de565794b2a09778920948b7e1c840615cfdcaa
Z <hernandezzuritadavid@gmail.com>
2025-05-14 12:53:41 -0500

    git remote add origin https://github.com/David-cod-Adr/Agenda_Onl.git git branch -M main git push -u origin main

--- app/src/main/java/com/example/angenda_online/MainActivity.java
+++ app/src/main/java/com/example/angenda_online/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.angenda_online;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button Btn_Login, Btn_Registro;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Btn_Login = findViewById(R.id.Btn_Login);
+        Btn_Registro = findViewById(R.id.Btn_Registro);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class ));
+            }
+        });
+        Btn_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
         });
     }
 }
